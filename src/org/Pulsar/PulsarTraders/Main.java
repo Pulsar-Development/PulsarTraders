ebba2a66fd9cae024148608079d8b8ab63a32541
NukerFall <carefall.regular@gmail.com>
2021-11-01 02:40:52 +0300

    Re Traders

--- src/org/Pulsar/PulsarTraders/Main.java
+++ src/org/Pulsar/PulsarTraders/Main.java
@@ -1,29 +1,5 @@
-package org.Pulsar.PulsarTraders;
+package org.pulsar.pulsartraders;
 
-import org.Pulsar.PulsarTraders.Commands.Manager;
-import org.Pulsar.PulsarTraders.Listeners.CitizensClick;
-import org.Pulsar.PulsarTraders.Listeners.InvHandler;
-import org.Pulsar.PulsarTraders.Listeners.PluginMsgListener;
-import org.bukkit.ChatColor;
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class Main extends JavaPlugin {
-	
-	public void onEnable() {
-		send("&aPlugin enabled!");
-		new CitizensClick(this);
-		new InvHandler(this);
-		new Manager(this);
-		getServer().getMessenger().registerOutgoingPluginChannel(this, "pulsar:system");
-		new PluginMsgListener(this);
-	}
-	
-	public void onDisable() {
-		send("&cPlugin disabled!");
-	}
-	
-	public void send(String s) {
-		getServer().getConsoleSender().sendMessage("[PulsarTraders] " + ChatColor.translateAlternateColorCodes('&', s));
-	}
+public class Main {
 
 }
